99416d49e387cb0d8fd325ddea478ab4b376df43
Masoud <masoud67.md@gmail.com>
2021-02-26 22:46:35 +0330

    bootcamp form exercise improved

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -8,6 +8,7 @@ function Form() {
 
   const [userNameInput, setUserNameInput] = useState("")
   const [passwordInput, setPasswordInput] = useState("")
+  const [invalid, setInvalid] = useState(false)
   const [auth, setAuth] = useState(false)
 
   function handleInput(e: React.BaseSyntheticEvent) {
@@ -21,7 +22,11 @@ function Form() {
     console.log(e.target.elements.username)
     if (e.target.elements.username.value === userInfo.username && e.target.elements.password.value === userInfo.password) {
       setAuth(true)
+      setInvalid(false)
       alert("Wellcome")
+    } else {
+      setInvalid(true)
+      setAuth(false)
     }
   }
   //whether id or name for inputs goes to e.target.elements
@@ -33,6 +38,7 @@ function Form() {
       <input type="text" id="password" value={passwordInput} onChange={handleInput} name="password" />
       <input type="submit"/>
       {auth && <p>Wellcome</p>}
+      {invalid && <p>Invalid input</p>}
     </form>
   )
 }
