cd8ed0c620dc8aa04447545ec69d2653d4901aa6
Masoud <masoud67.md@gmail.com>
2021-02-26 16:56:30 +0330

    new exercises of the session from bootcamp added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import LoginForm from "./components/login-form/LoginForm"
 import Counter from "./components/counter/Counter"
+import Form from "./components/form/Form"
 
 function App() {
   return (
-    <Counter maxCount={10} step={2} />
+    // <Counter maxCount={10} step={2} />
+    <Form />
   );
 }
 
---
+++ src/components/form/Form.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const userInfo = {
+  username: "username", password: "1234"
+}
+
+function Form() {
+
+  const [userNameInput, setUserNameInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
+  const [auth, setAuth] = useState(false)
+
+  function handleInput(e: React.BaseSyntheticEvent) {
+    console.log(e.target.name)
+    e.target.name === 'username' && setUserNameInput(e.target.value)
+    e.target.name === 'password' && setPasswordInput(e.target.value)
+  }
+
+  function submitHandle(e: React.BaseSyntheticEvent) {
+    e.preventDefault()
+    console.log(e.target.elements.username)
+    if (e.target.elements.username.value === userInfo.username && e.target.elements.password.value === userInfo.password) {
+      setAuth(true)
+      alert("Wellcome")
+    }
+  }
+  //whether id or name for inputs goes to e.target.elements
+  return (
+    <form onSubmit={(e) => submitHandle(e)}>
+      <label htmlFor="username"></label>
+      <input type="text" id="username" value={userNameInput} onChange={handleInput} name="username" />
+      <label htmlFor="password"></label>
+      <input type="text" id="password" value={passwordInput} onChange={handleInput} name="password" />
+      <input type="submit"/>
+      {auth && <p>Wellcome</p>}
+    </form>
+  )
+}
+
+export default Form
