895d4e3d99aa840ec2b37aee7904662dcad0a5df
Masoud <masoud67.md@gmail.com>
2021-03-10 19:51:55 +0330

    slight improvement in the theme-switcher exercise

--- src/components/theme-switcher/providers/ThemeProvider.jsx
+++ src/components/theme-switcher/providers/ThemeProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 import { THEME_TYPE } from "../constants/index";
 import Styles from "../data/Styles"
 
-export const ThemeContext = createContext({theme: "LIGHT"})
+export const ThemeContext = createContext({theme: "LIGHT", setTheme: () => {}})
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(THEME_TYPE.LIGHT)
